5418749dd90bdeb5a351a0ad549c7d5a6b6b30f6
mishaTytin <maliort165@gmail.com>
2023-10-17 21:39:11 +0300

    решение 2 задачи

---
+++ z2/Program.cs
@@ -0,0 +1,17 @@
+﻿//Напишите программу, которая выволит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.Write("Введите своё число -> ");
+string stringNumber = Console.ReadLine();
+
+int number;
+
+bool isNumber = int.TryParse(stringNumber, out number);
+ if (isNumber && stringNumber.Length >= 3)
+ {
+    Console.WriteLine("Ваше число подходит.");
+    Console.WriteLine($"Третья цифра числа {number} -> {stringNumber[2]}");
+ }
+ else
+ {
+    Console.WriteLine("В вашем числе есть буквы, либо его длинна не подходит для решения задачи.");
+ }
