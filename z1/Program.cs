bddcc10b32381de7b9ce761a9dd9dd2597315e57
mishaTytin <maliort165@gmail.com>
2023-10-17 21:23:08 +0300

    решение 1 задачи

---
+++ z1/Program.cs
@@ -0,0 +1,15 @@
+﻿//Напишите программу, которая на вход принимает трехзначное число и на выходе показывает вторую цифру этого числа
+
+Console.Write("Введите трехзначное число -> ");
+int number = Convert.ToInt32(Console.ReadLine());  //345
+
+if (number < 0)
+{
+    Console.WriteLine($"Ваше число {number} отрициательное, мы домножили его на -1");
+    number = number * -1;
+}
+
+int firstDigit = number % 100;                     //45
+int secondDigit = firstDigit / 10;                 //4
+
+Console.WriteLine($"Вторая цифра числа {number} -> {secondDigit}");
